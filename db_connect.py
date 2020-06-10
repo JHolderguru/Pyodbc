358cfd456a24830820e46b1b1b1655b8ef9d2566
JHolderguru <JHolder@spartaglobal.com>
2020-06-10 12:07:42 +0100

    merging SQL with Python exerices

--- db_connect.py
+++ db_connect.py
@@ -19,21 +19,45 @@ print(cursor)
 
 # 4)Fetchone() ---remember cursor maintains state
 query_result = cursor.execute('SELECT * FROM Products')
-print(query_result.fetchone()) #less one entry in the cursor\\\\\\\
-print(query_result.fetchone()) #less one entry in the cursor
-#if you want to get back to the start, you need a new cursor or to run the execute commang again
+
+print(query_result)
+
+
+# .fetchone() --> output next 1 pyodbc.Row -- remember cursor maintains state
+# if you want to get back to the start, you need a new cursor or to run the execute command again.
+print(query_result.fetchone()) # less one entry in the cursor
+print(query_result.fetchone()) # less one entry in the cursor \\\\\\\\\
 data_point_card = query_result.fetchone()
-#this one entry point of data
 
-all_results_list = query_result.fetchall()
-
-
-#this one entry of data is a pyodbc.Row object
+# this one entry of data is a 'pyodbc.Row' object
 print(type(data_point_card))
-#
-#behaves like a iterable list -organised with index
+# behaves like an iterable list - organised with index
 print(data_point_card[1])
+# also behave like a oop object, where the initialized parameters/attribute are the column names
+print(data_point_card.ProductName)
+
+# Search documentation/or internet to use a for loop to get our all of the data using the .fetchone() method
+
+
+# .fetchall --> output all the pyodbc.Row (s) into list -- remember cursor maintains state
+# DANGEROUS -- Do not use in real life. So also not here :)
+list_of_all_rows = query_result.fetchall()
+print(len(list_of_all_rows))
+# if you have a million entries this will cause your server to run out of intantanous memory and wil crash.
+
+# # print(query_result.fetchall())
+# # print(query_result.fetchall())
+# all_results_list = query_result.fetchall()
 #
+# for data in all_results_list:
+#     print(data.ProductName, 'costs: ', data.UnitPrice)
+
+
+
+# The Better way of getting out all your data
+# query_result = [10, 10, 100, 30, 50, None]
+
+# query_result = cursor.execute('SELECT UnitPrice FROM Products')
 
 query_result = cursor.execute('SELECT * FROM Products')
 
---
+++ new_db_connect.py
@@ -0,0 +1,43 @@
+import pyodbc
+
+# Make a new connection
+server = 'databases2.spartaglobal.academy'
+database = 'Northwind'
+username = 'SA'
+password = 'Passw0rd2018'
+
+db_string = 'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
+
+# making a new cursor
+py_connect2 = pyodbc.connect(db_string)
+cursor = py_connect2.cursor()
+print(cursor)
+
+# Q1 - How many orders in NWDB?
+# Query:
+query_result = cursor.execute('SELECT * FROM Orders ')
+
+# Response: 830
+# print(len(query_result.fetchall()))
+
+
+# Q2 - How many orders that the Ship City is Rio de Janeiro?
+# Query:
+query_result2 = cursor.execute("SELECT * FROM Orders"
+                               " WHERE ShipCity= 'Rio de Janeiro'")
+# Response: 34
+# print(len(query_result2.fetchall()))
+
+# Q3 - Select all orders that the Ship City is Rio de Janeiro or Reims?
+# Query:
+query_result3 = cursor.execute("SELECT * FROM Orders "
+                               "WHERE ShipCity = 'Rio de Janeiro' OR ShipCity= 'Reims' ")
+# Response:
+print(query_result3.fetchall())
+
+# Q4 - Select all of the entries where the Company name has a z or a Z in the table of Customers
+# Query:
+query_result4 = cursor.execute("SELECT * Customers WHERE CompanyName LIKE '%z%' OR CompanyName LIKE '%Z%' ")
+
+# Response:
+# print(query_result4.fetchall())
